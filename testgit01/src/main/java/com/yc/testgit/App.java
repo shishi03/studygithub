6da38de56a4af2462722c51348b00f4255999c4a
shishi03 <ms.ss@qq.com>
2016-05-19 20:05:50 +0800

    修改了APP.java代码

---
+++ testgit01/src/main/java/com/yc/testgit/App.java
@@ -0,0 +1,15 @@
+package com.yc.testgit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "我是修改的内容......." );
+        System.out.println( "我是重新修改的修改的内容......." );
+    }
+}
